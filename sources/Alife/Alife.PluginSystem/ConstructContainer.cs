ea34e9a81405f0c0128f35c9fd0658da8bc72e0e
BDFFZI <1330958515@qq.com>
2026-07-23 00:35:57 +0800

    完成基本的C#编译器、动态依赖注入容器、具有依赖树的插件上下文

---
+++ sources/Alife/Alife.PluginSystem/ConstructContainer.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Alife.PluginSystem;
+
+public class ConstructContainer
+{
+    public IReadOnlyList<object> Instances => instances;
+
+    public void RegisterBuilder(Type type, Func<Type, object>? builder = null)
+    {
+        builders.Add(type, builder ?? DefaultBuilder);
+    }
+    public void UnRegisterBuilder(Type type)
+    {
+        builders.Remove(type);
+    }
+
+    public void AddInstance(object instance)
+    {
+        instances.Add(instance);
+    }
+    public void RemoveInstance(object instance)
+    {
+        instances.Remove(instance);
+    }
+
+    public object RequireInstance(Type type)
+    {
+        object? instance = instances.FirstOrDefault(instance => instance.GetType().IsAssignableTo(type));
+        if (instance != null)
+            return instance;
+
+        Func<Type, object>? builder = builders.GetValueOrDefault(type.IsGenericType ? type.GetGenericTypeDefinition() : type);
+        if (builder != null)
+            instance = builder.Invoke(type);
+        else
+        {
+            foreach (var pair in builders)
+            {
+                Type compatibleType = pair.Key;
+                if (compatibleType.IsGenericTypeDefinition && type.IsGenericType)
+                    compatibleType = pair.Key.MakeGenericType(type.GetGenericArguments());
+
+                if (compatibleType.IsAssignableTo(type))
+                {
+                    instance = pair.Value.Invoke(compatibleType);
+                    break;
+                }
+            }
+        }
+        if (instance == null)
+            throw new Exception("无可用的构造器:" + type);
+
+        instances.Add(instance);
+        return instance;
+    }
+
+    readonly Dictionary<Type, Func<Type, object>> builders = new();
+    readonly List<object> instances = new();
+
+    object DefaultBuilder(Type type)
+    {
+        ConstructorInfo? constructor = type.GetConstructors().SingleOrDefault();
+        if (constructor == null)
+            throw new NotSupportedException("类型不支持构造:" + type);
+
+        object[] dependencies = constructor.GetParameters().Select(info => {
+            try
+            {
+                return RequireInstance(info.ParameterType);
+            }
+            catch (Exception)
+            {
+                if (info.HasDefaultValue)
+                    return info.DefaultValue!;
+                throw;
+            }
+        }).ToArray();
+        return constructor.Invoke(dependencies);
+    }
+}
---
+++ sources/Alife/Alife.PluginSystem/PluginLoadContext.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Runtime.Loader;
+
+namespace Alife.PluginSystem;
+
+public partial class PluginLoadContext
+{
+    static readonly Dictionary<string, Assembly> LoadedAssemblies = new();
+    static readonly Dictionary<Assembly, PluginLoadContext> AssemblyOwners = new();
+}
+
+public partial class PluginLoadContext(string name, string[] unmanagedDirectories) : AssemblyLoadContext(isCollectible: true, name: name), IDisposable
+{
+    public bool IsDisposing => isDisposing;
+    public void Complete()
+    {
+        foreach (Assembly assembly in Assemblies)
+        {
+            string? assemblyName = assembly.GetName().Name;
+            if (assemblyName == null)
+                continue;
+
+            LoadedAssemblies.Add(assemblyName, assembly);
+            AssemblyOwners.Add(assembly, this);
+        }
+    }
+    public void Dispose()
+    {
+        if (isDisposing)
+            return;
+        isDisposing = true;
+
+        foreach (PluginLoadContext childContext in childContexts)
+            childContext.Dispose();
+        foreach (PluginLoadContext parentContext in parentContexts)
+            parentContext.childContexts.Remove(this);
+        parentContexts.Clear();
+
+        foreach (Assembly assembly in Assemblies)
+        {
+            string? assemblyName = assembly.GetName().Name;
+            if (assemblyName == null)
+                continue;
+
+            LoadedAssemblies.Remove(assemblyName);
+            AssemblyOwners.Remove(assembly);
+        }
+        Unload();
+    }
+    public Assembly LoadDll(string dllPath)
+    {
+        string pdbPath = Path.ChangeExtension(dllPath, ".pdb");
+        var dllStream = new MemoryStream(File.ReadAllBytes(dllPath));
+        MemoryStream? pdbStream = File.Exists(pdbPath) ? new MemoryStream(File.ReadAllBytes(pdbPath)) : null;
+        Assembly assembly = LoadFromStream(dllStream, pdbStream);
+        dllStream.Dispose();
+        pdbStream?.Dispose();
+        return assembly;
+    }
+
+    protected override Assembly? Load(AssemblyName assemblyName)
+    {
+        if (assemblyName.Name != null && LoadedAssemblies.TryGetValue(assemblyName.Name, out Assembly? load))
+        {
+            var parent = AssemblyOwners[load];
+            parent.childContexts.Add(this);
+            parentContexts.Add(parent);
+
+            return load;
+        }
+
+        return base.Load(assemblyName);
+    }
+    protected override IntPtr LoadUnmanagedDll(string unmanagedDllName)
+    {
+        if (Path.HasExtension(unmanagedDllName) == false)
+            unmanagedDllName += ".dll";
+
+        foreach (string dir in unmanagedDirectories)
+        {
+            string[] candidatePaths = [
+                Path.Combine(dir, "runtimes", rid, "native", unmanagedDllName),
+                Path.Combine(dir, unmanagedDllName)
+            ];
+
+            foreach (string path in candidatePaths)
+            {
+                if (File.Exists(path))
+                    return LoadUnmanagedDllFromPath(path);
+            }
+        }
+
+        if (NativeLibrary.TryLoad(unmanagedDllName, out IntPtr handle))
+            return handle;
+
+        return IntPtr.Zero;
+    }
+
+    readonly HashSet<PluginLoadContext> childContexts = new();
+    readonly HashSet<PluginLoadContext> parentContexts = new();
+    bool isDisposing;
+
+    readonly string rid = RuntimeInformation.IsOSPlatform(OSPlatform.Windows)
+        ? $"win-{RuntimeInformation.OSArchitecture.ToString().ToLowerInvariant()}"
+        : RuntimeInformation.IsOSPlatform(OSPlatform.Linux)
+            ? $"linux-{RuntimeInformation.OSArchitecture.ToString().ToLowerInvariant()}"
+            : $"osx-{RuntimeInformation.OSArchitecture.ToString().ToLowerInvariant()}";
+}
